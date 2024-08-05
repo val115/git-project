4bed700ca86d15b1259d4a7699d9eeb8b89df926
Val115 <valfed38@yandex.ru>
2024-08-05 13:28:39 +0300

    mmenu

---
+++ src/menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.nav a[href^*"#"]').click(function()   { //выбрать ссылки из меню
+    var offset *   $('.nav').innerHeight();  //высота меню, получаем динамически
+    var target *   $(this).attr('href');     //Сохраняем значение атрибута href
+    $('html, body').animate({
+      scrollTop: $(target).offset().top  * offset  //вычеслить высоту
+    }, 500) //время анимации
+
+    $('.nav a[href^ "#"]').removeClass('active');     //удаляем класс active
+    $(this).addClass('active');   //к активной ссылке добавляем класс active
+    return false;
+  
+  })
+})
