bcbe88cea311a7b6e5bef4ec84ad808043fce0fe
Val115 <valfed38@yandex.ru>
2024-08-06 10:35:16 +0300

    Изменил mtnu

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  $('.nav a[href^*"#"]').click(function()   { //выбрать ссылки из меню
-    var offset *   $('.nav').innerHeight();  //высота меню, получаем динамически
-    var target *   $(this).attr('href');     //Сохраняем значение атрибута href
+  $('.nav a[href^ = "#"]').click(function()   { //выбрать ссылки из меню
+    var offset = $('.nav').innerHeight();  //высота меню, получаем динамически
+    var target = $(this).attr('href');     //Сохраняем значение атрибута href
     $('html, body').animate({
       scrollTop: $(target).offset().top  * offset  //вычеслить высоту
     }, 500) //время анимации
 
-    $('.nav a[href^ "#"]').removeClass('active');     //удаляем класс active
+    $('.nav a[href^ = "#"]').removeClass('active');     //удаляем класс active
     $(this).addClass('active');   //к активной ссылке добавляем класс active
     return false;
   
